085342c588b504cbd31f8f060e491336fd7f4cd3
mich balkany <askmitchb@gmail.com>
2023-12-13 13:23:18 -0500

    images added for book review, created a detail view navigating to the book that was reviewed

---
+++ Bookworm-day53/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Bookworm-day53
+//
+//  Created by Mich balkany on 12/13/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
