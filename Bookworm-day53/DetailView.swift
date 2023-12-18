a903f2a66d1220c01e0377ecd6d181177c294b4c
mich balkany <askmitchb@gmail.com>
2023-12-18 10:42:53 -0500

    books are sorted in specified order, saved and reviewed books are now editable from users list.

--- Bookworm-day53/DetailView.swift
+++ Bookworm-day53/DetailView.swift
@@ -5,14 +5,53 @@
 //  Created by Mich balkany on 12/13/23.
 //
 
+import SwiftData
 import SwiftUI
 
 struct DetailView: View {
+    let book: Book
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre.uppercased())
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+            
+            Text(book.review)
+                .padding()
+            
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
     }
 }
 
 #Preview {
-    DetailView()
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        let example = Book(title: "Test Book", author: "Author", genre: "Fantasy", review: "This was a fantastic book", rating: 4)
+        
+        return DetailView(book: example)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create preview: \(error.localizedDescription)")
+    }
 }
